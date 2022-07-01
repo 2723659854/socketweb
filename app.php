59245ab9f1f4022885f063c01263c0e05259ddc9
yanglong <2723659854@qq.com>
2022-07-01 17:01:19 +0800

    处理错误

--- app.php
+++ app.php
@@ -4,15 +4,47 @@ function handle($url,$param){
     list($file,$class,$method)=explode('@',$url);
     //todo 这种都不抛出异常，而是将错误记录然后渲染到一个文件上去
     if (!file_exists($file)){
-        throw new Exception($file.'文件不存在');
+        //throw new Exception($file.'文件不存在');
+        return  dispay('index',['msg'=>$file.'文件不存在']);
     }
     require_once $file;
     if (!class_exists($class)){
-        throw new Exception($class.'类不存在');
+        //throw new Exception($class.'类不存在');
+        return  dispay('index',['msg'=>$class.'类不存在']);
     }
     $class=new $class;
     if (!method_exists($class,$method)){
-        throw new Exception($method.'方法不存在');
+        //throw new Exception($method.'方法不存在');
+        return  dispay('index',['msg'=>$method.'方法不存在']);
     }
     return $class->$method($param);
+}
+
+function dispay($path,$param){
+    //首先是检测是否有对应的文件
+    //读取其中的内容
+    //根据定义的规则，在模板文件中找出变量，然后用户传的参数去替换
+    $content=file_get_contents('./error/'.$path.'.html');
+    if ($param){
+        //搜索以{开头}结尾的字符串，然后截取出来
+        $preg= '/{\$[\s\S]*?}/i';
+        preg_match_all($preg,$content,$res);
+        $array=$res['0'];
+        //组装用户传递的变量，方便检查变脸是否合法，以及渲染变量
+        $new_param=[];
+        foreach ($param as $k=>$v){
+            $key='{$'.$k.'}';
+            $new_param[$key]=$v;
+        }
+        //变量检查和渲染
+        foreach ($array as $k=>$v){
+            if (isset($new_param[$v])){
+                $content=str_replace($v,$new_param[$v],$content);
+            }else{
+                throw new Exception("未定义的变量".$v);
+            }
+        }
+    }
+
+    return $content;
 }
--- view.php
+++ view.php
@@ -1,4 +1,5 @@
 <?php
+
 //变脸的界定符,只能是单括号或者多括号{}或者{{}}，
 $rule=[
     'start'=>"{",
@@ -26,6 +27,35 @@ function view($path,$param){
             if (isset($new_param[$v])){
                 $content=str_replace($v,$new_param[$v],$content);
             }else{
+                //throw new Exception("未定义的变量".$v);
+               return no_declear('index',['msg'=>"未定义的变量".$v]);
+            }
+        }
+    }
+
+    return $content;
+}
+function no_declear($path,$param){
+    //首先是检测是否有对应的文件
+    //读取其中的内容
+    //根据定义的规则，在模板文件中找出变量，然后用户传的参数去替换
+    $content=file_get_contents('./error/'.$path.'.html');
+    if ($param){
+        //搜索以{开头}结尾的字符串，然后截取出来
+        $preg= '/{\$[\s\S]*?}/i';
+        preg_match_all($preg,$content,$res);
+        $array=$res['0'];
+        //组装用户传递的变量，方便检查变脸是否合法，以及渲染变量
+        $new_param=[];
+        foreach ($param as $k=>$v){
+            $key='{$'.$k.'}';
+            $new_param[$key]=$v;
+        }
+        //变量检查和渲染
+        foreach ($array as $k=>$v){
+            if (isset($new_param[$v])){
+                $content=str_replace($v,$new_param[$v],$content);
+            }else{
                 throw new Exception("未定义的变量".$v);
             }
         }
@@ -33,5 +63,4 @@ function view($path,$param){
 
     return $content;
 }
-
 //view('index',['var'=>"尼玛"]);
