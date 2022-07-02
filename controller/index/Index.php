46f627da5364d298e6d0f17349834e09938c3541
songshu <2723659854@qq.com>
2022-07-03 00:40:49 +0800

    一个稳定版本的php常驻内存框架

--- controller/index/Index.php
+++ controller/index/Index.php
@@ -19,7 +19,7 @@ class Index
         }
         $user=new User();
         $data=$user->where('username','=','test')->first();
-        print_r($data);
+        //print_r($data);
         $app_name=config('app')['app_name'];
         return view('index/index',['var'=>$var,'str'=>$str,'user'=>json_encode($data),'app_name'=>$app_name]);
     }
