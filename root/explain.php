d9be87763643f8499ff122600504ed4808958121
yanglong <2723659854@qq.com>
2022-07-04 18:00:29 +0800

    123

--- root/explain.php
+++ root/explain.php
@@ -55,12 +55,14 @@ class HttpServer
                 $param1=array_filter(explode(PHP_EOL,$str2));
                 $key=str_replace('"','',$param1[0]);
                 $value=str_replace('"','',$param1[2]);
+                var_dump($key);
+                var_dump($value);
                 $_param[$key]=$value;
                 $str3=substr($str1,stripos($str1,$part_end)+$part_end_length);
                 $need_str=$str3;
             }
 
-            var_dump($_param);
+            //var_dump($_param);
             //向接受的文件写入响应code
             socket_write($socketAccept, 'HTTP/1.1 200 OK' . PHP_EOL);
             //写入时间
