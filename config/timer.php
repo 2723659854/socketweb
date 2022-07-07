8470c7a021c0a386dad927334fdb5b99938e8e50
yanglong <2723659854@qq.com>
2022-07-07 14:28:06 +0800

    关闭定时器

--- config/timer.php
+++ config/timer.php
@@ -2,12 +2,12 @@
 return [
 
     //设置定时器
-    'test'=>[
-        'handle'=>\App\Time\Test::class,
-        'time'=>4,
-    ],
-    'other'=>[
-        'handle'=>\App\Time\OtherTimer::class,
-        'time'=>7,
-    ],
+//    'test'=>[
+//        'handle'=>\App\Time\Test::class,
+//        'time'=>4,
+//    ],
+//    'other'=>[
+//        'handle'=>\App\Time\OtherTimer::class,
+//        'time'=>7,
+//    ],
 ];
